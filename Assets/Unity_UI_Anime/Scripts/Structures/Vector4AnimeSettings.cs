c10f2f37089b4130b4b68c1d2c0a7e3f1c68ec3b
AmagiSakuya <AmagiSakuya@163.com>
2022-06-08 14:15:58 +0800

    修改

---
+++ Assets/Unity_UI_Anime/Scripts/Structures/Vector4AnimeSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sakuya.UnityUIAnime
+{
+    [System.Serializable]
+    public class Vector4AnimeSettings : BaseAnimeSettings
+    {
+        public Vector4 from;
+        public Vector4 to;
+    }
+}
